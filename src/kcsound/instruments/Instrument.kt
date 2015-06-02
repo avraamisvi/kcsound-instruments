68bc7d4a2efad947d7f85cddeb548f8123d452f4
abraao.isvi <abraao.isvi@gmail.com>
2015-06-02 17:39:38 -0300

    first commit

---
+++ src/kcsound/instruments/Instrument.kt
@@ -0,0 +1,27 @@
+import com.google.gson.JsonObject;
+
+/**
+ * Created by abraao on 6/2/15.
+ */
+interface Instrument {
+
+    /**
+     * Unique identification
+     */
+    val type: String;
+
+    /**
+     * Generate body content.
+     */
+    fun body():String;
+
+    /**
+     * Generate globals to be added to the score.
+     */
+    fun globals():String;
+
+    /**
+     * Compiles the instrument.
+     */
+    fun compile(data:JsonObject)
+}
